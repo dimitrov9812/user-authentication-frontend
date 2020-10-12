51867024819de8b686d4ca6bebc291838772c164
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-10-12 20:38:24 +0300

    build

---
+++ build/precache-manifest.ef94d0daf31bca7a38c5776f075e0898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2efd17cef303fc048fff222552d045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5526cf950ff7debbb95d",
+    "url": "/static/js/2.a77013c9.chunk.js"
+  },
+  {
+    "revision": "08c6fa4056a36119642c9ed680e51722",
+    "url": "/static/js/2.a77013c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1daf073a3b256fad533b",
+    "url": "/static/js/main.76cbd02e.chunk.js"
+  },
+  {
+    "revision": "10bf51df20d51006cc6d",
+    "url": "/static/js/runtime-main.419d7377.js"
+  }
+]);
